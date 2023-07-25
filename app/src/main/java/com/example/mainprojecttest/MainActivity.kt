e6f4d4965bc1138ff3d89001bf1cac1f0dbd411c
liurui <303996789@qq.com>
2023-07-25 13:45:29 +0800

    添加子module sub1

--- app/src/main/java/com/example/mainprojecttest/MainActivity.kt
+++ app/src/main/java/com/example/mainprojecttest/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mainprojecttest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.subprojecttest1.SubModuleMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initLogic()
+    }
+
+    private fun initLogic() {
+        var tv1 = findViewById<View>(R.id.tv1)
+        tv1.setOnClickListener {
+            var intent = Intent(this, SubModuleMainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
